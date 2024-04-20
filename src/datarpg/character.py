6cd5d76e53d0be2a25ce9aa8b62ebe7ce6ffdfdf
Giorgio Vilardo <giorgio@vilardo.eu>
2024-04-20 03:28:43 +0200

    feat: add factory methods

--- src/datarpg/character.py
+++ src/datarpg/character.py
@@ -1,4 +1,11 @@
 from dataclasses import dataclass
+from enum import StrEnum
+from typing import Any, Self
+
+
+class Role(StrEnum):
+    MELEE = "melee"
+    RANGED = "ranged"
 
 
 @dataclass(kw_only=True)
@@ -7,3 +14,15 @@ class Character:
     health: int = 1000
     level: int = 1
     is_dead: bool = False
+    role: Role = Role.MELEE
+    range: int = 2
+
+    @classmethod
+    def melee(cls: Self, **kwargs: dict[str, Any]) -> Self:
+        """Return a melee character with your options."""
+        return cls(**kwargs, role=Role.MELEE, range=2)
+
+    @classmethod
+    def ranged(cls: Self, **kwargs: dict[str, Any]) -> Self:
+        """Return a ranged character with your options."""
+        return cls(**kwargs, role=Role.RANGED, range=20)
