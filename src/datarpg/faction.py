07fa6671728893892e5498b7af7f16b4d0f60289
Giorgio Vilardo <giorgio@vilardo.eu>
2024-04-20 10:47:10 +0200

    can make alliances

--- src/datarpg/faction.py
+++ src/datarpg/faction.py
@@ -3,8 +3,8 @@ from typing import Any, ClassVar, Protocol
 
 
 class HasFactions(Protocol):
-    factions: list[str]
     __dataclass_fields__: ClassVar[dict[str, Any]]
+    factions: list[str]
 
 
 def join(joiner: HasFactions, faction: str) -> HasFactions:
@@ -15,3 +15,7 @@ def leave(joiner: HasFactions, faction: str) -> HasFactions:
     return dataclasses.replace(
         joiner, factions=[f for f in joiner.factions if f != faction]
     )
+
+
+def is_ally(member: HasFactions, other_member: HasFactions) -> bool:
+    return any(f in member.factions for f in other_member.factions)
--- tests/test_faction.py
+++ tests/test_faction.py
@@ -1,13 +1,35 @@
+from typing import cast
+
+import pytest
+
 from src.datarpg import faction
 from src.datarpg.character import Character
+from src.datarpg.faction import HasFactions
 
 
 def test_can_join_a_faction() -> None:
-    char = Character.melee(id=1)
+    char: HasFactions = cast(HasFactions, Character.melee(id=1))
     assert faction.join(char, "TestFaction").factions == ["TestFaction"]
 
 
 def test_can_leave_a_faction() -> None:
-    char = Character.melee(id=1)
+    char: HasFactions = cast(HasFactions, Character.melee(id=1))
     assert faction.join(char, "TestFaction").factions == ["TestFaction"]
     assert faction.leave(char, "TestFaction").factions == []
+
+
+@pytest.mark.parametrize(
+    ("char_1", "char_2", "expected"),
+    [
+        (Character.melee(id=1), Character.ranged(id=2), True),
+    ],
+)
+def test_can_spot_alliances(
+    *,
+    char_1: HasFactions,
+    char_2: HasFactions,
+    expected: bool,
+) -> None:
+    char_1 = faction.join(char_1, "TestFaction")
+    char_2 = faction.join(char_2, "TestFaction")
+    assert faction.is_ally(char_1, char_2) is expected
