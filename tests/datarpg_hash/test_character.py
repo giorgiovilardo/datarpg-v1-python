e397391b3299c2f0668d8ff308de88c0b068e60d
Giorgio Vilardo <giorgio@vilardo.eu>
2024-04-21 14:04:20 +0200

    fail test for range

--- tests/datarpg_hash/test_character.py
+++ tests/datarpg_hash/test_character.py
@@ -4,3 +4,7 @@ from src.datarpg.datarpg_hash import character
 def test_default_character() -> None:
     expected = {"health": 1000, "level": 1, "is_dead": False}
     assert all(kv in character.default() for kv in expected)
+
+
+def test_characters_have_range() -> None:
+    assert "range" in character.default()
