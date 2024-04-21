6fc41326292a90d2a7f377a2f3f69aac0b9eed27
Giorgio Vilardo <giorgio@vilardo.eu>
2024-04-21 15:01:14 +0200

    red cycle: factions join/leave'

--- tests/datarpg_hash/test_character.py
+++ tests/datarpg_hash/test_character.py
@@ -7,6 +7,20 @@ def test_default_character() -> None:
 
 
 def test_characters_have_range() -> None:
-    assert "range" in character.default()
     assert character.default()["range"] == character.MELEE_RANGE
     assert character.default_ranged()["range"] == character.RANGED_RANGE
+
+
+def test_characters_have_factions_but_start_with_none() -> None:
+    assert not character.has_factions(character.default())
+    assert not character.has_factions(character.default_ranged())
+
+
+def test_characters_can_join_or_leave_factions() -> None:
+    char = character.default()
+    assert not character.has_factions(char)
+    char = character.join_faction(char, "test_faction")
+    assert character.has_factions(char)
+    assert "test_faction" in char["factions"]
+    char = character.leave_faction(char, "test_faction")
+    assert not character.has_factions(char)
