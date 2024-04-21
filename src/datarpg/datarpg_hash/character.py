4f1b1c997941eb671319b0823ee0c8d9888a016f
Giorgio Vilardo <giorgio@vilardo.eu>
2024-04-21 15:14:20 +0200

    add factions

--- src/datarpg/datarpg_hash/character.py
+++ src/datarpg/datarpg_hash/character.py
@@ -1,8 +1,15 @@
 from typing import TypedDict
 
 
-class Character(TypedDict):
+class HasName(TypedDict):
     name: str
+
+
+class HasFactions(TypedDict):
+    factions: list[str]
+
+
+class Character(HasName, HasFactions):
     health: int
     level: int
     is_dead: bool
@@ -20,6 +27,7 @@ def default() -> Character:
         "level": 1,
         "is_dead": False,
         "range": MELEE_RANGE,
+        "factions": [],
     }
 
 
@@ -30,13 +38,28 @@ def default_ranged() -> Character:
         "level": 1,
         "is_dead": False,
         "range": RANGED_RANGE,
+        "factions": [],
     }
-
-
-class HasName(TypedDict):
-    name: str
 
 
 def is_the_same(a: HasName, b: HasName) -> bool:
     # public function, should be validated
     return a["name"] == b["name"]
+
+
+def has_factions(char: HasFactions) -> bool:
+    return len(char["factions"]) > 0
+
+
+def join_faction(char: HasFactions, faction: str) -> HasFactions:
+    new_factions = (
+        char["factions"] + [faction]
+        if faction not in char["factions"]
+        else char["factions"]
+    )
+    return char | {"factions": new_factions}
+
+
+def leave_faction(char: HasFactions, faction: str) -> HasFactions:
+    new_factions = [f for f in char["factions"] if f != faction]
+    return char | {"factions": new_factions}
