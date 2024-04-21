1d5d2c07e91e181a0fde3df4c5eceb4a84072349
Giorgio Vilardo <giorgio@vilardo.eu>
2024-04-21 15:19:41 +0200

    red cycle: are allies

--- tests/datarpg_hash/test_character.py
+++ tests/datarpg_hash/test_character.py
@@ -24,3 +24,13 @@ def test_characters_can_join_or_leave_factions() -> None:
     assert "test_faction" in char["factions"]
     char = character.leave_faction(char, "test_faction")
     assert not character.has_factions(char)
+
+
+def test_characters_can_be_allies() -> None:
+    char_1 = character.default()
+    char_2 = character.default_ranged()
+    char_1 = character.join_faction(char_1, "test_faction")
+    char_2 = character.join_faction(char_2, "test_faction")
+    assert character.are_allies(char_1, char_2)
+    char_2 = character.leave_faction(char_2, "test_faction")
+    assert not character.are_allies(char_1, char_2)
